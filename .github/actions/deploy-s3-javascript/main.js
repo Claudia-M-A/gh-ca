6a76ff2d1d97865972c21b78207f53e36e703d34
claudia <claud.sg.11@gmail.com>
2026-09-03 19:44:10 +1000

    Added log

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -12,7 +12,10 @@ function run() {
   const s3Uri = `s3://${bucket}`;
   exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
 
-  core.notice("Deploying to AWS S3 from my custom javasript action ...");
+  const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
+  core.setOutput("website-url", websiteUrl);
+
+  core.notice("Deploying to AWS S3 from my custom javascript action ...");
 }
 
 run();
